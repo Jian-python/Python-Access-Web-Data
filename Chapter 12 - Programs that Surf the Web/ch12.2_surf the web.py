316e813363db49785c2e490697eaae2b7fb44ed6
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-04-01 13:09:55 +1100

    Create ch12.2_surf the web.py

---
+++ Chapter 12 - Programs that Surf the Web/ch12.2_surf the web.py
@@ -0,0 +1,68 @@
+print('chapter 12.2 Programs that Surf the Web')
+print('***********part 1***********')
+print('1. representing simple strings')
+print('the "ord()" function tells the numeric value of a simple ASCII character')
+# each character is represented by a number between 0 and 256 stored in 8 bits of memory
+# we prefer '8 bits of memory' as 'a "byte" of memory'
+print('H:', ord('H'))
+print('h:', ord('h'))
+print('e:', ord('e'))
+print('#####################################################')
+print('2. multi-byte characters')
+# UTF-16: fixed length - 2 bytes
+# UTF-32: fixed length - 4 bytes
+# UTF-8: 1-4 bytes (recommended practice for exchange data between systems)
+print('#####################################################')
+print('3. Python strings to bytes')
+# when send bytes to external resources like a network socket: encode Python 3 into given character encoding
+# when read data from external resource, we decode it based on the character set so it represented in python 3 as string
+# code:===========================
+# while True:
+#     data = mysock.recv(512)
+#     if (len(data)<1):
+#         break
+#     mystring = data.decode() <-decode: bytes to unicode
+#     print(mystring)
+# =================================
+print('#####################################################')
+print('4. Using urllib in Python')
+# urllib does all socket work and makes web paegs look like a  file
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip()) #strip() remove the newline between lines
+print('#####################################################')
+print('5. like a file...')
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+count = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        count[word] = count.get(word, 0) + 1
+print(count)
+print('#####################################################')
+print('6. reading web pages')
+# read HTML file
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
+# output:==============
+# <h1>The First Page</h1>
+# <p>
+# If you like, you can switch to the
+# <a href="http://www.dr-chuck.com/page2.htm">
+# Second Page</a>.
+# </p>
+# ======================
+print('#####################################################')
+print('7. web scraping')
+# program / script pretends to be web browser and restrieves web pages, llok at those pages,
+#  extracts info, and look at more web pages
+
+print('web scrawling or spidering the web:','\n','search engines scrape web pages', sep='')
+print('#####################################################')
